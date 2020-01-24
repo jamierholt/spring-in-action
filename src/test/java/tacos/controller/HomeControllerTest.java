6258d6331a801237998adc9866ea0adf3f99d2a3
Jamie Holt <jamierholt@gmail.com>
2020-01-24 13:47:04 -0700

    Expanded controllers and templates, added form validation

--- src/test/java/tacos/controller/HomeControllerTest.java
+++ src/test/java/tacos/controller/HomeControllerTest.java
@@ -12,7 +12,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @ExtendWith(SpringExtension.class)
-@WebMvcTest(HomeController.class)
+@WebMvcTest
 public class HomeControllerTest {
 
     @Autowired
